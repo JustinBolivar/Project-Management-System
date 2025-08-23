27031c674195fe4cf17ca3e274b27ce2f474f603
JustinBolivar <justinantonio.bolivar@gmail.com>
2025-08-23 16:55:39 +0800

    Fixed a bug where routes were not setup properly because of a namespace problem and php not being setup properly

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
+    // Project Routes
+    Route::apiResource('projects', ProjectController::class);
+
+    // Task Routes (nested under projects)
+    Route::apiResource('projects.tasks', TaskController::class)->shallow();
+});
