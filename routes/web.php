9291587c53b9b628e6495cea68100d516fb7c4d4
JustinBolivar <justinantonio.bolivar@gmail.com>
2025-08-24 16:16:07 +0800

    fixed bug where API endpoints returns frontend HTML

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Catch-all for React/Vite, but exclude /api/*
 Route::get('/{any}', function () {
     return view('welcome');
-})->where('any', '.*');
+})->where('any', '^(?!api).*');
