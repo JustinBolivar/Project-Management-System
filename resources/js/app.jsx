c130ace4b2c8ba6d6061b2be1baf5d67c5588d0c
JustinBolivar <justinantonio.bolivar@gmail.com>
2025-08-24 13:07:43 +0800

    Added the routes of the new pages to make sure the screen uses the correct pages

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,17 +1,41 @@
+import './bootstrap'; // This is standard for Laravel
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
+// Import your main CSS file (where Tailwind is imported)
+import '../css/app.css';
+
+// Import the pages we created
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+
+// The main App component now acts as the Router
 const App = () => {
     return (
-        <div>
-            <h1>Hello from React! 👋</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                {/* Redirect the base URL to the login page */}
+                <Route path="/" element={<Navigate to="/login" />} />
+
+                {/* Define the routes for your pages */}
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+
+                {/* You can add a signup route later */}
+                {/* <Route path="/signup" element={<SignupPage />} /> */}
+            </Routes>
+        </BrowserRouter>
     );
 };
 
-// Find the HTML element where your React app will be rendered
+// This part stays the same from your old file!
 const container = document.getElementById('app');
 if (container) {
     const root = createRoot(container);
-    root.render(<App />);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
 }
